3c561ede03ebc7f1ef19b4931a56b5d8b4421f59
Sara Evans <saraevs@gmail.com>
2021-02-22 18:05:30 +0000

    added a create method to the yurts controller

--- app/controllers/yurts_controller.rb
+++ app/controllers/yurts_controller.rb
@@ -10,6 +10,25 @@ class YurtsController < ApplicationController
   end
 
   def new
+    # raise
+    # @user = User.find(params[:user_id])
     @yurt = Yurt.new
+  end
+
+  def create
+    @yurt = Yurt.new(yurt_params)
+    @yurt.user = current_user
+    @yurt.save
+    if @yurt.save
+      redirect_to yurts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def yurt_params
+    params.require(:yurt).permit(:title, :description, :location, :max_capacity, :day_rate)
   end
 end
