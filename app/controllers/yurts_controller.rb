faca8b2d4a9f666c8d0b2961c803be5cc5d0e738
Sara Evans <saraevs@gmail.com>
2021-02-22 16:21:48 +0000

    added some css and flexbox

--- app/controllers/yurts_controller.rb
+++ app/controllers/yurts_controller.rb
@@ -1,5 +1,6 @@
 class YurtsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  # dependent on users controller feature
+  # skip_before_action :authenticate_user!, only: :index
 
   def index
     @yurts = Yurt.all
