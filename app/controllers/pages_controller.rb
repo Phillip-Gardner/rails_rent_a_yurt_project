29b9362c8156dfe67f4980b644ff329675e72c84
Sara Evans <saraevs@gmail.com>
2021-02-23 15:08:22 +0000

    added profile route and method to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
-class PagesController < ApplicationController\
-
-skip_before_action :authenticate_user!, only: :home
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
 
   def home
+  end
+
+  def profile
+    @user = current_user
+    @yurts = Yurt.where(user: @user)
   end
 end
