eacd0a6379eb3c5c119bf7b9798dac4420d1c6e5
Eliza Plowden <elizaplowden@gmail.com>
2021-02-23 16:48:37 +0000

    seeded database - added 10 users, 5 yurts and 2 bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,55 +7,63 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require "open-uri"
+require "faker"
 Booking.destroy_all
 Yurt.destroy_all
 User.destroy_all
-
+puts 'creating 5 users and yurts'
 
 new_user = User.create(email: 'email@email.com', password: 'Password1')
 user2 = User.create(email: 'hello@hello.com', password: '123Password')
 user3 = User.create(email: 'Kurt@gmail.com', password: 'Password!')
 user4 = User.create(email: 'yurt_owner@email.com', password: 'iloveyurts')
 user5 = User.create(email: 'email@yahoo.com', password: 'password1')
-user6 = User.create(email: 'hello@hotmail.co.uk', password: 'Password!')
-user7 = User.create(email: 'email@email.co.uk', password: 'tents1234')
-user8 = User.create(email: 'bonjour@hello.com', password: 'frenchie10')
-user9 = User.create(email: 'john@johns.fr', password: 'Password2')
-user10 = User.create(email: 'hi@hotmail.co.uk', password: 'password123')
-
 
 new_yurt1 = Yurt.new(title:"Teepee", description: "A great TeePee", max_capacity: 10, location:"Suffolk", day_rate:"55")
 new_yurt1.user = new_user
 new_yurt1.save
 
-
 new_yurt2 = Yurt.new(title:"Wedding Marque", description: "Brilliant for getting married", max_capacity: 100, location:"Kent", day_rate:"2800")
-new_yurt2.user = user7
+new_yurt2.user = user4
 new_yurt2.save
 
-new_yurt3 = Yurt.new(title:"Kurt's Yurt", description: "My old yurt", max_capacity: 4, location:"Devon", day_rate:"5")
-new_yurt3.user = user8
-new_yurt3.save
-
-new_yurt4 = Yurt.new(title: "Best Yurt",)
-new_yurt4.user = user5
-
-file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
-article = Article.new(title: 'NES', body: "A great console")
-article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-
-new_yurt4.save
-
-
-
-
-
 new_booking = Booking.create(booked_from: '28/07/2021', booked_to: '29/07/2021', total_cost: '250', user: user3, yurt: new_yurt2)
-new_booking = Booking.create(booked_from: '01/03/2021', booked_to: '10/03/2021', total_cost: '35', user: user10, yurt: new_yurt1)
+new_booking = Booking.create(booked_from: '01/03/2021', booked_to: '10/03/2021', total_cost: '35', user: user5, yurt: new_yurt1)
+
+
+# puts "Destroy users"
+# User.destroy_all if Rails.env.development?
+title = ["Lovely Yurt", "Kurt's Yurt", "Best Yurt", "Stunning Teepee", "Gorgeous Tent"]
+description = ["I can guarantee you won't find a nicer Yurt in the South", "My old yurt", "A Great teepee", "Brilliant for getting married", "stunning yurt in the countryside"]
+location = ["Surrey", "Kent", "Yorkshire", "Highlands", "Devon", "Cornwall", "Wiltshire", "Suffolk"]
+# user_filenames = ['nicolas-horn-MTZTGvDsHFY-unsplash_xpfqge.jpg', 'ben-parker-NohB3FJSY90-unsplash_bgq5zz.jpg', 'ben-parker-OhKElOkQ3RE-unsplash_cbbhi5.jpg', 'luis-villasmil-6qf1uljGpU4-unsplash_xvyzar.jpg', 'sarah-brown-tTdC88_6a_I-unsplash_wkvvpl.jpg']
+yurt_photos =
+['https://res.cloudinary.com/dlxlmoq7d/image/upload/v1614098398/4Yurt_qq7byn.jpg',
+'https://res.cloudinary.com/dlxlmoq7d/image/upload/v1614098394/3Yurt_mvbwzq.jpg',
+'https://res.cloudinary.com/dlxlmoq7d/image/upload/v1614098391/2Yurt_jrqjfy.jpg',
+'https://res.cloudinary.com/dlxlmoq7d/image/upload/v1614098388/1Yurt_rblxqp.jpg',
+'https://res.cloudinary.com/dlxlmoq7d/image/upload/v1614093588/z8amus5p9g4don1sqet116gstp10.jpg']
+yurt_filenames = ['4Yurt_qq7byn.jpgg', '3Yurt_mvbwzq.jpgg',
+ '2Yurt_jrqjfy.jpg', '1Yurt_rblxqp.jpg', 'z8amus5p9g4don1sqet116gstp10.jpg']
+
+
+
+counter = 0
+5.times do
+  # user_file = URI.open("#{user_photos[counter]}")
+  # add host=true once added to model
+  user = User.create!(email: Faker::Internet.email, password: '123456')
+  # user.photo.attach(io: user_file, filename: "#{user_filenames[counter]}", content_type: 'image/jpg')
+  yurt_file = URI.open("#{yurt_photos[counter]}")
+  yurt = Yurt.new(title: "#{title[counter]}", description: "#{description[counter]}", location:"#{location[counter]}", max_capacity: Faker::Number.within(range: 1..250), day_rate:Faker::Number.within(range: 20..100), user_id: user.id )
+  yurt.photo.attach(io: yurt_file, filename: "#{yurt_filenames[counter]}", content_type: 'image/jpg')
+  yurt.save
+  counter += 1
+end
 
 
 # Yurt.create!(title:"Teepee", description: "A great TeePee", max_capacity: 10, location:"Suffolk", day_rate:"55")
 
-puts "#{User.all.count} users created"
-puts "#{Yurt.all.count} yurts created"
-puts "#{Booking.all.count} booking created"
+# puts "#{User.all.count} users created"
+# puts "#{Yurt.all.count} yurts created"
+# puts "#{Booking.all.count} booking created"
