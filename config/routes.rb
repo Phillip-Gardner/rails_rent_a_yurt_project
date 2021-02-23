dfca6115c985cb3b6686d81cb10879cb7439830e
george murphy <georgeolivermurphy@hotmail.com>
2021-02-23 15:49:47 +0000

    Added a destroy bookings feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :yurts do
     resources :bookings, only: [:index, :new, :create]
   end
-  resources :bookings, only: [:show, :index]
+  resources :bookings, only: [:show, :index, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,16 @@
 
 new_user = User.create(email: 'email@email.com', password: 'Password1')
 
-new_booking = Booking.create(booked_from: '28/07/2021', booked_to: '29/07/2021', total_cost: '250', user_id: 1, yurt_id: 1)
-
-
 # Yurt.create!(title:"Teepee", description: "A great TeePee", max_capacity: 10, location:"Suffolk", day_rate:"55")
 
-new_yurt1 = Yurt.new(title:"Teepee", description: "A great TeePee", max_capacity: 10, location:"Suffolk", day_rate:"55")
-new_yurt1.user = new_user
+new_yurt1 = Yurt.new(title:"Teepee", description: "A great TeePee", max_capacity: 10, location:"Suffolk", day_rate:"55", user_id: 1)
 new_yurt1.save
 
 
-new_yurt2 = Yurt.new(title:"Wedding Marque", description: "Brilliant for getting married", max_capacity: 100, location:"Kent", day_rate:"2800")
-new_yurt2.user = new_user
+new_yurt2 = Yurt.new(title:"Wedding Marque", description: "Brilliant for getting married", max_capacity: 100, location:"Kent", day_rate:"2800", user_id: new_user.id)
 new_yurt2.save
 
-new_yurt3 = Yurt.new(title:"Kurt's Yurt", description: "My old yurt", max_capacity: 4, location:"Devon", day_rate:"5")
-new_yurt3.user = new_user
+new_yurt3 = Yurt.new(title:"Kurt's Yurt", description: "My old yurt", max_capacity: 4, location:"Devon", day_rate:"5", user_id: new_user.id)
 new_yurt3.save
 
+new_booking = Booking.create(booked_from: '28/07/2021', booked_to: '29/07/2021', total_cost: '250', user_id: new_user.id, yurt_id: 1)
